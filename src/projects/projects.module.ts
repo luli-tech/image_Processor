ca51c633738e1a4bb5fbc39f5ab03abcf3549df0
luli-tech <olabodemicheal5@gmail.com>
2025-12-07 00:00:11 +0100

    other service integration

--- src/projects/projects.module.ts
+++ src/projects/projects.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { ProjectsController } from './projects.controller';
 import { ProjectsService } from './projects.service';
+import { Project, ProjectSchema } from '../mongooseShema/project.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Project.name, schema: ProjectSchema }])
+  ],
   controllers: [ProjectsController],
-  providers: [ProjectsService]
+  providers: [ProjectsService],
+  exports: [ProjectsService],
 })
 export class ProjectsModule {}
